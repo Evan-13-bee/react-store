179854c243a990dd90ab85f4218b6027e0ff0522
evan <evanzsmit@gmail.com>
2021-08-15 09:44:53 +0700

    Updates

---
+++ precache-manifest.8f3020ff1b1e9e1e6bdc59b665c8b62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1509cadca37ae7c86fc1d5e5d6b1ee",
+    "url": "/react-store/index.html"
+  },
+  {
+    "revision": "0dd53c2fc30ea8a9490f",
+    "url": "/react-store/static/css/main.1ad58a78.chunk.css"
+  },
+  {
+    "revision": "caed12667f4e06a264e0",
+    "url": "/react-store/static/js/2.9009f1e8.chunk.js"
+  },
+  {
+    "revision": "3bb8f4be79104c3617df3eac6c3c9584",
+    "url": "/react-store/static/js/2.9009f1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd53c2fc30ea8a9490f",
+    "url": "/react-store/static/js/main.2d0e7a6b.chunk.js"
+  },
+  {
+    "revision": "fd1e9c55823268e49fa9",
+    "url": "/react-store/static/js/runtime-main.b2ed3988.js"
+  }
+]);
