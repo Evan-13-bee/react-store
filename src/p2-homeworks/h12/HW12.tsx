c82128ff2521aaa0d3659d42e3c9b9900466a00a
evan <evanzsmit@gmail.com>
2021-08-15 13:26:05 +0700

    12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,26 +1,42 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppStoreType } from "../h10/bll/store";
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
+import { changeThemeC } from "./bll/themeReducer";
 import s from "./HW12.module.css";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+  const theme = useSelector<AppStoreType, string>(store => store.theme.theme); // useSelector
+  // useDispatch, onChangeCallback
+  const dispatch = useDispatch()
+  const onChangeCallback = (color: string) => {
+    dispatch(changeThemeC(color))
+  }
 
-    // useDispatch, onChangeCallback
+  return (
+    <div className={s[theme]}>
+      <hr />
+      <span className={s[theme + '-text']}>
+        homeworks 12
+      </span>
 
-    return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
-            </span>
+      {/*should work (должно работать)*/}
+      {/*SuperSelect or SuperRadio*/}
+      <br />
+      <div className={s.wrapper}>
+        <SuperRadio
+          name={'radio'}
+          options={themes}
+          value={theme}
+          onChangeOption={onChangeCallback}
+        />
+      </div>
 
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
-
-            <hr/>
-        </div>
-    );
+      <hr />
+    </div>
+  );
 }
 
 export default HW12;
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,20 @@
 const initState = {
-
+    theme: ''
 };
-
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type ThemeActionType = {
+    type: string
+    color: string
+}
+export const themeReducer = (state = initState, action: ThemeActionType): typeof initState => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "CHANGE_THEME": {
+            const newState = {...state, theme: action.color}
+            return newState;
         }
         default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export const changeThemeC = (color: string): ThemeActionType => {
+    return {type: 'CHANGE_THEME', color}
+}; // fix any
